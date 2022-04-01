0f19c0800b4266611ab0b75f1f4ecbbdf1aa3748
Michael Forero <mforeroroldan@gmail.com>
2022-03-31 21:15:09 -0500

    imagenes actualizadas

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -1,32 +1,35 @@
-import { faIceCream, faPlateWheat, faUtensils, faWineGlass } from '@fortawesome/free-solid-svg-icons'
+import { faIceCream, faUtensils, faWineGlass } from '@fortawesome/free-solid-svg-icons'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import React from 'react'
 import { Tab, Tabs } from 'react-bootstrap'
-import Frecuentes from '../images/Frecuentes'
-import Productos from './Productos'
+import { Platos, Bebidas, Postres } from "../data/data"
+
 
 const Menu = () => {
 
-    const platos = Frecuentes.map(({ img, name, value }) => (
-        <div>
-            <div class="box-event-modern">
+    const itemsMenu = (productos) => {
+        const Menu = productos.map(({ img, name, value }) => (
+            <div>
+                <div class="box-event-modern">
 
-                <div className="event-item-modern articulo">
-                    <div id='imgArticulo'>
-                        <img src={img} width="100px" />
-                    </div>
-                    <div id='desArticulo'>
+                    <div className="event-item-modern articulo">
+                        <div id='imgArticulo'>
+                            <img src={img} width="100px" alt='imagenes'/>
+                        </div>
+                        <div id='desArticulo'>
 
-                        <p className="event-time">{value}</p>
-                        <h4 className="event-item-modern-title"><a href="#">{name}</a></h4>
-                        <div className="event-item-modern-text">
-                            <p>The most popular drink in Venice: refreshing, easygoing &…happy! Perfect to be sipped as an “Aperitivo” just before dinner - delightful!</p>
+                            <p className="event-time">{value}</p>
+                            <h4 className="event-item-modern-title"><a href="/">{name}</a></h4>
+                            <div className="event-item-modern-text">
+                                <p>The most popular drink in Venice: refreshing, easygoing &…happy! Perfect to be sipped as an “Aperitivo” just before dinner - delightful!</p>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
-    ))
+        ))
+        return Menu
+    }
 
     return (
         <div>
@@ -39,20 +42,20 @@ const Menu = () => {
 
 
                                 <Tabs defaultActiveKey="profile" id="uncontrolled-tab-example" className="justify-content-center TabMenu">
-                                    <Tab eventKey="home" title={<FontAwesomeIcon icon={faUtensils}/>}>
+                                    <Tab eventKey="home" title={<FontAwesomeIcon icon={faUtensils} />}>
                                         <div className="row row-20 row-lg-30">
-                                            {platos}
+                                            {itemsMenu(Platos)}
                                         </div>
                                     </Tab>
-                                    <Tab eventKey="profile" title={<FontAwesomeIcon icon={faWineGlass}/>}>
+                                    <Tab eventKey="profile" title={<FontAwesomeIcon icon={faWineGlass} />}>
                                         <div className="row row-20 row-lg-30">
-                                            {platos}
+                                            {itemsMenu(Bebidas)}
                                         </div>
                                     </Tab>
-                                    <Tab eventKey="contact" title={<FontAwesomeIcon icon={faIceCream}/>}>
+                                    <Tab eventKey="contact" title={<FontAwesomeIcon icon={faIceCream} />}>
                                         <div className="row row-20 row-lg-30">
                                             <br />
-                                            {platos}
+                                            {itemsMenu(Postres)}
                                         </div>
                                     </Tab>
                                 </Tabs>
--- src/data/data.js
+++ src/data/data.js
@@ -1,10 +1,152 @@
+import arroz from "../images/productos/platos/Arroz.jpg"
+import cerAgridulce from "../images/productos/platos/CerdoAgridulce.jpg"
+import churrasco from "../images/productos/platos/Churrasco.jpg"
+import costillas from "../images/productos/platos/CostillasBBq.jpg"
+import pechuga from "../images/productos/platos/PechugaPlancha.jpg"
+import baSalsa from "../images/productos/platos/BagreSalsa.jpg"
+import Mojarra from "../images/productos/platos/Mojarra.jpg"
+import MojarraAsada from "../images/productos/platos/MojarraAsada.jpg"
+import Mojarrita from "../images/productos/platos/Mojarrita.jpg"
+import robalo from "../images/productos/platos/RobaloMarinero.jpg"
+import caSudad from "../images/productos/platos/CachamaSudada.jpg"
+import cazuela from "../images/productos/platos/Cazuela.jpg"
+import salchipapa from "../images/productos/platos/Salchipapa.jpg"
+import sancocho from "../images/productos/platos/Sancocho.jpg"
+import Hamburguesa from "../images/productos/platos/Hamburguesa.jpg"
+import brownie from "../images/productos/postres/PBrownie.jpg"
+import flan from "../images/productos/postres/PFlanMandarina.jpg"
+import gelatina from "../images/productos/postres/PGelatina.jpg"
+import cocacola from "../images/productos/bebidas/cocacola.jpg"
+import cafe from "../images/productos/bebidas/coffee.jpg"
+import vino from "../images/productos/bebidas/vino.jpg"
+
+
+
 const Contactme = {
-    whatsapp:"https://api.whatsapp.com/send?phone=573158419824&text=hola%20", 
-    facebook:"https://www.facebook.com/Centro-Ecoturistico-Canaima-105501684381882",
-    instagram:"https://www.instagram.com/centroecoturisticocanaima/",
+    whatsapp: "https://api.whatsapp.com/send?phone=573158419824&text=hola%20",
+    facebook: "https://www.facebook.com/Centro-Ecoturistico-Canaima-105501684381882",
+    instagram: "https://www.instagram.com/centroecoturisticocanaima/",
     googleMaps: "https://goo.gl/maps/VRa7QTCziQefAvZp9"
 }
 
+const Platos = [
+    {
+        img: sancocho,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: arroz,
+        name: "producto1",
+        value: 50000
+    },
+    {
+        img: cerAgridulce,
+        name: "producto3",
+        value: 50000
+    },
+    {
+        img: churrasco,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: costillas,
+        name: "producto1",
+        value: 50000
+    },
+    {
+        img: pechuga,
+        name: "producto3",
+        value: 50000
+    },
+    {
+        img: baSalsa,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: Mojarra,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: MojarraAsada,
+        name: "producto3",
+        value: 50000
+    },
+    {
+        img: Mojarrita,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: robalo,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: caSudad,
+        name: "producto3",
+        value: 50000
+    },
+    {
+        img: cazuela,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: Hamburguesa,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: salchipapa,
+        name: "producto3",
+        value: 50000
+    }
+]
+
+const Postres = [
+    {
+        img: brownie,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: gelatina,
+        name: "producto1",
+        value: 50000
+    },
+    {
+        img: flan,
+        name: "producto3",
+        value: 50000
+    },
+]
+
+const Bebidas = [
+
+    {
+        img: cocacola,
+        name: "producto4",
+        value: 50000
+    },
+    {
+        img: cafe,
+        name: "producto1",
+        value: 50000
+    },
+    {
+        img: vino,
+        name: "producto3",
+        value: 50000
+    },
+]
 export {
-    Contactme
+    Contactme,
+    Platos, 
+    Bebidas,
+    Postres
+
 }
